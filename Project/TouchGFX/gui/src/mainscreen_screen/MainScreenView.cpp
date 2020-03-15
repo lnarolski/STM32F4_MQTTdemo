3a77dd21b17335376b1ad810328a0efcccdfe58d
lnarolski <narolskilukasz+github@gmail.com>
2020-03-15 16:55:46 +0100

    Added UART test in MainScreenView

--- Project/TouchGFX/gui/src/mainscreen_screen/MainScreenView.cpp
+++ Project/TouchGFX/gui/src/mainscreen_screen/MainScreenView.cpp
@@ -7,43 +7,102 @@
 #define BUFFER_SIZE 2001
 
 UART_HandleTypeDef huart7;
-uint8_t ReceivedChar;
+uint8_t ReceivedChar[15];
 
 extern uint32_t BlueDuty;
 
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
-	
+static HAL_StatusTypeDef UART_WaitOnFlagUntilTimeout(UART_HandleTypeDef *huart, uint32_t Flag, FlagStatus Status, uint32_t Tickstart, uint32_t Timeout)
+{
+  /* Wait until flag is set */
+  while((__HAL_UART_GET_FLAG(huart, Flag) ? SET : RESET) == Status) 
+  {
+    /* Check for the Timeout */
+    if(Timeout != HAL_MAX_DELAY)
+    {
+      if((Timeout == 0U)||((HAL_GetTick() - Tickstart ) > Timeout))
+      {
+        /* Disable TXE, RXNE, PE and ERR (Frame error, noise error, overrun error) interrupts for the interrupt process */
+        CLEAR_BIT(huart->Instance->CR1, (USART_CR1_RXNEIE | USART_CR1_PEIE | USART_CR1_TXEIE));
+        CLEAR_BIT(huart->Instance->CR3, USART_CR3_EIE);
+        
+        huart->gState  = HAL_UART_STATE_READY;
+        huart->RxState = HAL_UART_STATE_READY;
+        
+        /* Process Unlocked */
+        __HAL_UNLOCK(huart);
+        
+        return HAL_TIMEOUT;
+      }
+    }
+  }
+  
+  return HAL_OK;
 }
 
 void MainScreenView::handleTickEvent()
 {
-//	if (huart7.RxState == HAL_UART_STATE_READY)
-//	{
-//		ReceivedChar = huart7.Instance
-//		
-//		static bool inited = false;
-//		static char buffer[BUFFER_SIZE];
-//		static uint32_t i = 0;
-//		
-//		if (!inited)
-//		{
-//			memset(buffer, '\0', BUFFER_SIZE);
-//		}
-//		
-//		buffer[i] = ReceivedChar;
-//		++i;
-//		
-//		if (strstr(buffer, "TEST") != NULL)
-//		{
-//			BlueDuty = 100;
-//			memset(buffer, '\0', BUFFER_SIZE);
-//		}
-//		if (i > BUFFER_SIZE -1)
-//		{
-//			i = 0;
-//			memset(buffer, '\0', BUFFER_SIZE);
-//		}
-//	}
+	// My implementation of HAL_UART_Receive() function
+	if (huart7.RxState == HAL_UART_STATE_READY)
+	{		
+		/* Process Locked */
+    do{                                        \
+			if(huart7.Lock == HAL_LOCKED)   \
+			{                                      \
+				 return;		                  \
+			}                                      \
+			else                                   \
+			{                                      \
+				 huart7.Lock = HAL_LOCKED;    \
+			}                                      \
+		} while (0U);
+    
+    huart7.ErrorCode = HAL_UART_ERROR_NONE;
+    huart7.RxState = HAL_UART_STATE_BUSY_RX;
+		
+		static bool inited = false;
+		static char buffer[BUFFER_SIZE];
+		static uint32_t i = 0;
+		
+		if (!inited)
+		{
+			memset(buffer, '\0', BUFFER_SIZE);
+		}
+		
+		/* Init tickstart for timeout managment */
+    uint32_t tickstart = HAL_GetTick();
+		uint32_t timeout = 100;
+		
+		while (UART_WaitOnFlagUntilTimeout(&huart7, UART_FLAG_RXNE, RESET, tickstart, timeout) == HAL_OK)
+		{
+			buffer[i] = (uint8_t)(huart7.Instance->DR & (uint8_t)0x00FF);
+			inited = true;
+			
+			if (strstr(buffer, "TEST") != NULL)
+			{
+				BlueDuty = 100;
+				memset(buffer, '\0', BUFFER_SIZE);
+				i = 0;
+				inited = false;
+			}
+			else if (i > BUFFER_SIZE -1 || buffer[i] == '\n')
+			{
+				i = 0;
+				memset(buffer, '\0', BUFFER_SIZE);
+				inited = false;
+			}
+			else
+			{
+				++i;
+			}
+		}
+		
+		/* At end of Rx process, restore huart->RxState to Ready */
+    huart7.RxState = HAL_UART_STATE_READY;
+		
+		do {                                       \
+			huart7.Lock = HAL_UNLOCKED;    \
+		} while (0U);
+	}
 }
 
 static void MX_UART7_Init(void)
@@ -114,9 +173,9 @@ MainScreenView::MainScreenView()
 	if (!inited)
 	{
 		MX_UART7_Init();
-		HAL_UART_MspInit(&huart7);
+		//HAL_UART_MspInit(&huart7);
 		HAL_UART_Transmit(&huart7,(uint8_t*) "SUBS$demo\n", strlen("SUBS$demo\n"), 1000);
-		HAL_UART_Receive_IT(&huart7, &ReceivedChar, 1);
+		//HAL_UART_Receive_IT(&huart7, &ReceivedChar, 1);
 		
 		inited = true;
 	}
